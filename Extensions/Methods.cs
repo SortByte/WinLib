6ef4d25db1a0f95e5ac917f0ba9e376dcd5225a3
CristianVladescu <chris_db@outlook.com>
2016-12-03 13:16:49 +0200

    Add project files.

---
+++ Extensions/Methods.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Collections.Concurrent;
+
+namespace WinLib.Extensions
+{
+    public static class Methods
+    {
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = System.Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr =
+                           Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Attempts to remove, without returning the value that has the specified key from the <see cref="System.Collections.Concurrent.ConcurrentDictionary{TKey, TValue}"/> 
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            TValue ignored;
+            return dictionary.TryRemove(key, out ignored);
+        }
+        /// <summary>
+        /// If you use this extension on a dictionary to add entries, don't mix it with other methods (like TryAdd) in multi-threading environment
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool AddToEnd<TValue>(this ConcurrentDictionary<int, TValue> dictionary, TValue value)
+        {
+            lock (dictionary)
+            {
+                return dictionary.TryAdd(Enumerable.Range(0, int.MaxValue).Except(dictionary.Keys).First(), value);
+            }
+        }
+        public static T CreateInstance<T>(T type) where T : new()
+        {
+            return new T();
+        }
+        public static T CastObject<T>(object obj, T type) where T : class
+        {
+            return obj as T;
+        }
+    }
+
+    //class HelperClass
+    //{
+    //    public static T CreateInstance<T>(T type) where T : new()
+    //    {
+    //        return new T();
+    //    }
+    //}
+    //class MyType1 { }; class MyType2 { }; class MyType3 { };
+    //class Main
+    //{
+    //    public Main()
+    //    {
+    //        Type[] myTypes = new Type[] { typeof(MyType1), typeof(MyType2), typeof(MyType3) };
+    //        Type myType = myTypes[new Random().Next(1,3)];
+    //        var obj1 = HelperClass.CreateInstance(myType);
+    //        var obj2 = Activator.CreateInstance(myType);
+    //    }
+    //}
+}
---
+++ Forms/LoadingForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinLib.Forms
+{
+    public partial class LoadingForm : Form
+    {
+        /// <summary>
+        /// Starts a splash loading form on a new thread running a separate message pump
+        /// </summary>
+        public LoadingForm(string status)
+        {
+            InitializeComponent();
+            label1.Text = status;
+            Thread thread = new Thread(new ThreadStart(Start));
+            thread.Start();
+        }
+
+        public void Start()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(this);
+        }
+
+        public void UpdateStatus(string status)
+        {
+            while (!IsHandleCreated)
+                Thread.Sleep(100);
+            Invoke(new Action(() => { label1.Text = status; }));
+        }
+
+        public void UpdateProgress(int percent)
+        {
+            while (!IsHandleCreated)
+                Thread.Sleep(100);
+            if (percent < 0)
+                Invoke(new Action(() => {
+                    progressBar1.Style = ProgressBarStyle.Marquee;
+                    progressBar1.Value = 33;
+                }));
+            else
+                Invoke(new Action(() => {
+                    progressBar1.Style = ProgressBarStyle.Continuous;
+                    progressBar1.Value = percent; }));
+        }
+
+        public void Stop()
+        {
+            while (!IsHandleCreated)
+                Thread.Sleep(100);
+            Invoke(new Action(() => { Close(); }));
+        }
+    }
+}
